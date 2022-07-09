26bb9ecb260c58c004075d0e34dff3441efa3c8b
AlexSzeto <Alex.Szeto@gmail.com>
2022-07-08 20:21:55 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,102 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "survivor":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 8 8 8 . . . . . . 
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. 8 8 . . . 8 8 8 8 . . . . . . 
+. 8 8 8 . . . 8 8 . . . . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . . . 8 8 8 8 8 . . . . . 
+. . . . . . 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . . 8 8 8 8 8 8 . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . 8 8 8 . . 8 8 8 . . . . 
+. . . 8 8 8 . . . . 8 8 8 . . . 
+`;
+            case "image2":
+            case "ghost":return img`
+. . . 1 1 1 1 1 1 . . . . . . . 
+. . 1 1 1 1 1 1 1 1 1 . . . . . 
+. . 1 . 1 1 1 1 1 . 1 . . . . . 
+. . 1 . . 1 1 1 . . 1 . . . . . 
+. . 1 . 1 1 1 1 . . 1 . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+1 1 1 1 1 . . . 1 1 1 1 1 . . . 
+. 1 1 1 . . . . . 1 1 1 1 . . . 
+. . 1 1 . . . . . 1 1 1 . . . . 
+. . 1 1 1 . . . 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . . . . 1 1 1 1 1 . . . . 
+. . . . . . . . . 1 1 1 1 . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . 2 2 . . . 
+. . . . . . . . 2 2 2 2 . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . 2 4 4 4 2 . . . . . . 
+. . . . 2 4 4 4 4 4 2 . . . . . 
+. . . . 2 4 5 5 5 4 2 . . . . . 
+. . . . 2 4 5 5 5 4 2 . . . . . 
+. . . . 2 4 5 5 5 4 2 . . . . . 
+. . . . . 2 4 4 4 2 . . . . . . 
+. . . . . . 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "dark dialog frame":return img`
+..bbbbbbbbbbbbbbbbbbbb..
+.bdccccccccccccccccccdb.
+bdcdbbbbbbbbbbbbbbbbdcdb
+bcdbbbbbbbbbbbbbbbbbbdcb
+bcbdccccccccccccccccdbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbccccccccccccccccccbcb
+bcbdccccccccccccccccdbcb
+bdcbbbbbbbbbbbbbbbbbbcdb
+bbdccccccccccccccccccdbb
+.bbbbbbbbbbbbbbbbbbbbbb.
+..bbbbbbbbbbbbbbbbbbbb..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
